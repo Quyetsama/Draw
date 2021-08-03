0de07e4991e8c8acae78d13c9b202577d7c02e0b
Quyetsama <you@example.com>
2021-08-03 15:07:14 +0700

    1

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+
+app.use(express.static('public'))
+app.set("view engine", "ejs");
+
+
+app.get('/', (req, res, next) => {
+    res.render('home')
+})
+
+server.listen("3000", function(){
+    console.log("Server is running!!!");
+});
+
+
+io.on('connection', (socket) => {
+    console.log('Client connected: ' + socket.id)
+    socket.on('mouse', (data) => {
+        console.log(data)
+        socket.broadcast.emit('mouse', data)
+    })
+    socket.on('disconnect', () => console.log('Client has disconnected'))
+})
